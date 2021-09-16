cdc0760f937c804d684ed5586fd0eac3ef4e1ec6
Ilya Atrakhimenok <ilya.atrakhimenok@mail.ru>
2021-09-16 10:55:31 +0300

    feat: add adaptive header and welcome section

---
+++ museum/js/header.js
@@ -0,0 +1,113 @@
+let burger = document.getElementById("header__burger");
+let cancel = document.getElementById("header__cancel");
+let nav = document.querySelector(".header__nav");
+let text = document.querySelector(".welcome__text");
+let menuItems = document.querySelectorAll(".header__item");
+let headerConatcts = document.querySelector(".header__contacts");
+let headerImgs = document.querySelector(".header__imgs");
+let headerInner = document.querySelector(".header-inner");
+
+
+async function showMenu() {
+  console.log('showMenu');
+  if (window.innerWidth > 768) {
+    burger.style.display = "none";
+    cancel.style.display = "block";
+    text.style.display = "none";
+    nav.style.display = "block";
+    for (let i = 0; i < menuItems.length; i++) {
+      await delay(220).then(() => {
+        menuItems[i].style.display = "block";
+        menuItems[i].animate(
+          [{ transform: "translateX(-100%)" }, { transform: "translateX(0)" }],
+          { duration: 220 }
+        );
+      });
+    }
+  } else {
+    burger.style.display = "none";
+    cancel.style.display = "block";
+    headerConatcts.style.display = "flex";
+    headerInner.style.display = "grid";
+    headerImgs.style.display = "grid";
+    nav.style.display = "block";
+    for (let i = 0; i < menuItems.length; i++) {
+      await delay(220).then(() => {
+        menuItems[i].style.display = "block";
+        menuItems[i].animate(
+          [{ transform: "translateX(-100%)" }, { transform: "translateX(0)" }],
+          { duration: 220 }
+        );
+      });
+    }
+  }
+}
+
+async function cancelMenu() {
+  await smoothlyHideNav();
+  if (window.innerWidth > 768) {
+    await delay(500).then(() => {
+      text.style.display = "block";
+      text.animate([{ opacity: "0" }, { opacity: "100%" }], {
+        duration: 500,
+        easing: "ease-in-out",
+      });
+    });
+    hideMenu();
+  } else {
+    headerConatcts.style.display = "none";
+    headerInner.style.display = "flex";
+    headerImgs.style.display = "none";
+    hideMenu();
+  }
+}
+
+function changeAdaptive() {
+  if (window.innerWidth > 1024) {
+    nav.style.display = "block";
+    burger.style.display = "none";
+    cancel.style.display = "none";
+    menuItems.forEach((item) => {
+      item.style.display = "block";
+    });
+  } else {
+    hideMenu();
+  }
+}
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+async function smoothlyHideNav(){
+  for (let i = 0; i < menuItems.length; i++) {
+    await delay(220).then(() => {
+      menuItems[i].animate(
+        [{ transform: "translateX(0)" }, { transform: "translateX(-100%)" }],
+        { duration: 220 }
+      );
+      menuItems[i].style.transform = "translateX(-200%)";
+    });
+  }
+}
+function hideMenu() {
+  console.log('hidemenu');
+  nav.style.display = "none";
+  cancel.style.display = "none";
+  burger.style.display = "block";
+  text.style.display = "block";
+  menuItems.forEach((item) => {
+    item.style.display = "none";
+    item.style.transform = "translateX(0)";
+  });
+}
+window.addEventListener("click", function (event) {
+  if (
+    nav.style.display == "block" &&
+    event.target.className != "header__item" &&
+    event.target.className != "fas fa-bars"
+  ) {
+    cancelMenu();
+  }
+});
+window.addEventListener("resize", changeAdaptive);
+cancel.addEventListener("click", cancelMenu);
+burger.addEventListener("click", showMenu);
--- museum/js/welcome.js
+++ museum/js/welcome.js
@@ -1,11 +1,14 @@
-let slider = document.querySelector('.welcome__slider');
-let counter = document.querySelector('.welcome__slider-counter');
-let leftArrow = document.querySelector('.welcome__arrow-left');
-let rightArrow = document.querySelector('.welcome__arrow-right');
-let welcomeInner = document.querySelector('.welcome-inner');
-let squares = document.querySelectorAll('.slider-square');
-let imgs = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'];
+let slider = document.querySelector(".welcome__slider");
+let counter = document.querySelector(".welcome__slider-counter");
+let leftArrow = document.querySelector(".welcome__arrow-left");
+let rightArrow = document.querySelector(".welcome__arrow-right");
+let welcomeInner = document.querySelector(".welcome-inner");
+let squares = document.querySelectorAll(".slider-square");
+let welcomeImgs = document.querySelectorAll(".welcome__img");
+let imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 let timerId = null;
+let leftX,
+  rightX = 0;
 function findActiveIndex(arr, classActive) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].classList.contains(classActive)) {
@@ -13,62 +16,129 @@ function findActiveIndex(arr, classActive) {
     }
   }
 }
-function changeCounter(){
-  counter.innerHTML = `0${findActiveIndex(squares,'active-square') + 1} | 05`
+function changeCounter() {
+  counter.innerHTML = `0${findActiveIndex(squares, "active-square") + 1} | 05`;
 }
-function changeImg(){
+function changeImg(direction) {
   clearTimeout(timerId);
-  let activeIndex = findActiveIndex(squares,'active-square');
-  welcomeInner.style.background = ` linear-gradient(
-    to right,
-    rgba(0, 0, 0, 1) 30%,
-    rgba(0, 0, 0, 0) 60%
-  ),
-  url(/assets/img/welcome-slider/${imgs[activeIndex]}) 100% 100% no-repeat`;
-  welcomeInner.classList.add('run-animation');
-  timerId = setTimeout(() => welcomeInner.classList.remove('run-animation'),700);
+  let activeIndex = findActiveIndex(squares, "active-square");
+  if (window.innerWidth > 768) {
+    welcomeInner.style.backgroundImage = `url(/museum/assets/img/welcome-slider/${imgs[activeIndex]})`;
+    if (direction == "left") {
+      welcomeInner.animate(
+        [
+          { backgroundPositionX: `${welcomeInner.clientWidth}px` },
+          { backgroundPositionX: "100%" },
+        ],
+        {
+          duration: 700,
+          easing: "ease-in-out",
+        }
+      );
+    } else {
+      welcomeInner.animate(
+        [
+          { backgroundPositionX: `-${window.innerWidth / 2.7}px` },
+          { backgroundPositionX: "100%" },
+        ],
+        {
+          duration: 800,
+          easing: "ease-in-out",
+        }
+      );
+    }
+  }
+  else{
+    let activeIndexImg = findActiveIndex(welcomeImgs, "welcome__active");
+    welcomeImgs[activeIndexImg].classList.remove('welcome__active');
+    welcomeImgs[activeIndex].classList.add('welcome__active');
 
+    if (direction == "left") {
+      welcomeImgs[activeIndex].animate(
+        [
+          { transform: `translateX(100%)` },
+          { transform: "translateX(0)" },
+        ],
+        {
+          duration: 700,
+          easing: "ease-in-out",
+        }
+      );
+    } else {
+      welcomeImgs[activeIndex].animate(
+        [
+          { transform: `translateX(-100%)` },
+          { transform: "translateX(0)" },
+        ],
+        {
+          duration: 700,
+          easing: "ease-in-out",
+        }
+      );
+    }
+  }
 }
 function moveNext() {
-    console.log('next');
   let activeIndex = findActiveIndex(squares, "active-square");
 
   if (activeIndex == squares.length - 1) {
     squares[activeIndex].classList.remove("active-square");
     squares[0].classList.add("active-square");
-
   } else {
     squares[activeIndex].classList.remove("active-square");
     squares[activeIndex + 1].classList.add("active-square");
   }
   changeCounter();
-  changeImg();
+  changeImg("right");
 }
 function movePrev() {
-    console.log('prev');
   let activeIndex = findActiveIndex(squares, "active-square");
 
   if (activeIndex == 0) {
     squares[activeIndex].classList.remove("active-square");
     squares[squares.length - 1].classList.add("active-square");
-
   } else {
     squares[activeIndex].classList.remove("active-square");
     squares[activeIndex - 1].classList.add("active-square");
   }
   changeCounter();
-  changeImg();
+  changeImg("left");
 }
 
 function changeActiveElement(elem) {
+  let direct = null;
   let activeIndex = findActiveIndex(squares, "active-square");
   squares[activeIndex].classList.remove("active-square");
   elem.classList.add("active-square");
+  direct = activeIndex - findActiveIndex(squares, "active-square");
+  if (direct > 0) changeImg("left");
+  else changeImg("right");
   changeCounter();
-  changeImg();
 }
-
+function controlShadow() {
+  if (window.innerWidth > 1500)
+    welcomeInner.style.boxShadow = `inset ${500}px 0px 100px 0px rgba(0, 0, 0, 1)`;
+  else if (window.innerWidth > 768) {
+    welcomeInner.style.boxShadow = `inset ${
+      window.innerWidth / 3
+    }px 0px 100px 0px rgba(0, 0, 0, 1)`;
+  } else {
+    welcomeInner.style.boxShadow = `none`;
+  }
+}
+function mouseDown(event) {
+  document.body.style.cursor = "grab";
+  leftX = event.clientX;
+}
+function mouseUp(event) {
+  document.body.style.cursor = "default";
+  rightX = event.clientX;
+  if (leftX - rightX > 200) movePrev();
+  else if (rightX - leftX > 200) moveNext();
+}
+controlShadow();
+window.addEventListener("resize", controlShadow);
 rightArrow.addEventListener("click", moveNext);
 leftArrow.addEventListener("click", movePrev);
-
-
+welcomeInner.addEventListener("mousedown", mouseDown);
+welcomeInner.addEventListener("mouseup", mouseUp);
