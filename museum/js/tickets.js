d5132e703926e92eb7fc9dab5ea788344355effd
Ilya Atrakhimenok <ilya.atrakhimenok@mail.ru>
2021-09-27 20:24:07 +0300

    feat: add tickets section, add forms

---
+++ museum/js/tickets.js
@@ -0,0 +1,74 @@
+let modal = document.querySelector('.modal');
+let closeBtn = document.querySelector('.close');
+let ticketsBtn = document.querySelector('.tickets__total-btn');
+let inputBasic = document.querySelectorAll('.input-basic');
+let inputSenior = document.querySelectorAll('.input-senior');
+let tickets__total = document.querySelector('.tickets__total');
+let overviewTotal = document.querySelector('.overview-total-total')
+
+let overviewBasic = document.querySelector('.overview-basic-amount');
+let overviewSenior = document.querySelector('.overview-senior-amount');
+
+let basicTotal = document.querySelector('.overview-basic-total');
+let seniorTotal = document.querySelector('.overview-senior-total');
+
+
+
+function changeTicketsAmount(event){
+    if(event.className == 'tickets__btn minus-basic' && inputBasic[0].value > 0){
+        inputBasic[0].value--;
+        inputBasic[1].value--;
+    }
+    else if(event.className == 'tickets__btn plus-basic' && inputBasic[0].value < 20){
+        console.log(inputBasic[0].value, inputBasic[1].value);
+        inputBasic[0].value++;
+        inputBasic[1].value++;
+    }
+    else if(event.className == 'tickets__btn minus-senior' && inputSenior[0].value > 0){
+        inputSenior[0].value--;
+        inputSenior[1].value--;
+    }
+    else if(event.className == 'tickets__btn plus-senior' && inputSenior[0].value < 20){
+        inputSenior[0].value++;
+        inputSenior[1].value++;
+    }
+    tickets__total.innerHTML = `Total €${inputBasic[0].value * 20 + inputSenior[0].value * 10}`;
+    overviewTotal.innerHTML = `${inputBasic[0].value * 20 + inputSenior[0].value * 10} €`;
+    basicTotal.innerHTML = `${inputBasic[0].value * 20} €`;
+    seniorTotal.innerHTML = `${inputSenior[0].value * 10} €`;
+    overviewBasic.innerHTML = inputBasic[0].value;
+    overviewSenior.innerHTML = inputSenior[0].value;
+
+}
+
+
+function openModal(){
+    modal.style.display = 'flex';
+    modal.animate([
+        {
+            transform: "translateX(-100%)" 
+        },
+        {
+            transform: "translateX(0)"
+        }
+    ], {duration: 400})
+}
+function closeModal(){
+    modal.animate([
+        {
+            transform: "translateX(0)" 
+        },
+        {
+            transform: "translateX(-100%)"
+        }
+    ], {duration: 400})
+    setTimeout(()=>{
+        modal.style.display = 'none';
+    },400);
+}
+ticketsBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+modal.addEventListener('click',(event)=>{
+    if(event.target.className == 'modal')
+        closeModal();
+})
